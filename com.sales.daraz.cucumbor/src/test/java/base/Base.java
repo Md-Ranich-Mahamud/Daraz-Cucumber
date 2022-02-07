d25477a9fbf113fae8b3a7b016a694553c186b70
Md Ranich Mahamud <99212549+Md-Ranich-Mahamud@users.noreply.github.com>
2022-02-07 15:03:26 -0500

    BaseClassUpdated

--- com.sales.daraz.cucumbor/src/test/java/base/Base.java
+++ com.sales.daraz.cucumbor/src/test/java/base/Base.java
@@ -4,19 +4,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Base {
-	
+	// Declaration of WebDriver variable
 	public static WebDriver driver;
 	
-	
+	// This is a navigateTo method
 	public static void navigateTo(String url) {
 		driver.get(url);
 	}
 	
+	// This is a click method
 	public static void click(By by) {
 		driver.findElement(by).click();
 	}
 	
-	
+	// This is a sendKeys method
 	public static void sendKeys(By by, String searchTag) {
 		driver.findElement(by).sendKeys(searchTag);
 	}
